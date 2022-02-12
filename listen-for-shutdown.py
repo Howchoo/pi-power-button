afdd2c88f7706d2cf452fbbe1b3adca6141483aa
ImpressiveTaste <94606213+ImpressiveTaste@users.noreply.github.com>
2022-02-12 10:07:32 +0100

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -3,10 +3,21 @@
 
 import RPi.GPIO as GPIO
 import subprocess
-
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+
+stay=1 #incidcates if shutdown hasbeen sent correctly
+
+while stay:
+	GPIO.wait_for_edge(3, GPIO.FALLING) #button is pressed
+	for i in range(30): #wait for a second
+		time.sleep(0.1)
+		if GPIO.input(3) == 1: #if button not pressed
+			stay=1
+			break
+		else:
+			stay=0
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
